1139a1707d0a05e94f14ac09521cd59304e3c977
Rodrigo Vanhoz <rodrigo.vanhoz@gmail.com>
2020-02-24 18:16:39 -0300

    Consulta com vários critérios

--- src/main/java/com/rodvanhoz/workshopmongo/services/PostService.java
+++ src/main/java/com/rodvanhoz/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.rodvanhoz.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,4 +39,9 @@ public class PostService {
 		return repo.searchTitle(text);
 	}
 	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSerach(text, minDate, maxDate);
+	}
 }
