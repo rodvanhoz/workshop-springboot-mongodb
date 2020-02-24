5b6dfc4e30fad628fba6e53743a1d3aaaec26d2c
Rodrigo Vanhoz <rodrigo.vanhoz@gmail.com>
2020-02-23 22:31:37 -0300

    Obtendo um usuário por id

--- src/main/java/com/rodvanhoz/workshopmongo/services/UserService.java
+++ src/main/java/com/rodvanhoz/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.rodvanhoz.workshopmongo.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodvanhoz.workshopmongo.domain.User;
 import com.rodvanhoz.workshopmongo.repository.UserRepository;
+import com.rodvanhoz.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,23 @@ public class UserService {
 	public List<User> findAll() {
 	
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		
+		try {
+			
+			User user = repo.findById(id).get();
+			if (user == null) {
+				
+				throw new ObjectNotFoundException("Objeto nao encontrado");
+			}
+			
+			return user;
+		}
+		catch (Exception e) {
+			throw new ObjectNotFoundException("Objeto nao encontrado");
+		}
+		
 	}
 }
